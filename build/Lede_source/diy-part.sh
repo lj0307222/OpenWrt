3606642ce656291587ca9c56e062b625525d9786
lj0307222 <61595433+lj0307222@users.noreply.github.com>
2022-10-28 00:01:56 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -45,6 +45,10 @@ sed -i 's/ca-bundle luci-app-wireguard//g' ./target/linux/x86/Makefile
 sed -i '/^kmod-usb-net/d' ./target/linux/x86/Makefile
 sed -i '/^kmod-sound-hda-core/d' ./target/linux/x86/Makefile
 
+# 查看kakefile
+cat ./target/linux/x86/Makefile
+
+
 # 编译多主题时,设置固件默认主题（可自行修改您要的,主题名称必须对,比如下面代码的[argon],和肯定编译了该主题,要不然进不了后台）
 #sed -i "/exit 0/i\uci set luci.main.mediaurlbase='/luci-static/argon' && uci commit luci" "${FIN_PATH}"
 
