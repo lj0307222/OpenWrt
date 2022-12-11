2d7a78fd53e8849249ccdd29b6c88d98c23ceeb2
lj0307222 <61595433+lj0307222@users.noreply.github.com>
2022-12-11 15:58:38 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -12,7 +12,7 @@ sed -i '/^kmod-usb-net/d' ./target/linux/x86/Makefile
 sed -i 's/pciutils/pciutils kmod-r8168 htop lm-sensors iperf3 autocore-x86 automount kmod-drm-amdgpu/g' ./target/linux/x86/Makefile
 
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-dnsfilter package/dnsfilter
-svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-socat package/socat
+# svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-socat package/socat
 git clone https://github.com/linkease/istore.git package/store
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-tencentddns package/ddns
 svn co https://github.com/kenzok8/small-package/trunk/luci-app-openclash package/openclash
